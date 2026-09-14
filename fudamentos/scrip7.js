55b2a96d6c831fded28833d054cf5b64f9063bdd
JOSED4VIDM <josedavidmarin311@gmail.com>
2026-09-14 10:41:15 -0500

    Delete fudamentos directory

--- fudamentos/scrip7.js
+++
@@ -1,9 +0,0 @@
-// Ejercicio 7: Comprobar si un número es par o impar
-
-let numero = parseInt(prompt("Ingrese un número:"));
-
-if (numero % 2 == 0) {
-    document.write("El número " + numero + " es par.");
-} else {
-    document.write("El número " + numero + " es impar.");
-}
--- fudamentos/scrip8.js
+++
@@ -1,9 +0,0 @@
-let año = parseInt(prompt("Ingrese un año:"));
-
-if ((año % 4 == 0 && año % 100 != 0) || (año % 400 == 0)) {
-    document.getElementById("resultado").innerHTML =
-        "El año " + año + " es bisiesto.";
-} else {
-    document.getElementById("resultado").innerHTML =
-        "El año " + año + " no es bisiesto.";
-}
